e4c02bcdcebc7bcce9f056d4558996668bf88d22
ricky02242247 <61371711+ricky02242247@users.noreply.github.com>
2020-03-14 11:04:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,3 @@ def diary(c):
         contents.append((name, page, i))
     return render_template("page.html", cs = contents)
 
-server.run(debug=True,
-           host="0.0.0.0",
-           port="3000")
